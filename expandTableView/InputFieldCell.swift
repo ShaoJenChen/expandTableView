eeb227c658230a4e5e16faabe178fe37839a2bee
shaojen <shaojen@geovision.com.tw>
2019-04-19 18:30:05 +0800

    add inputFieldCell

--- expandTableView/InputFieldCell.swift
+++ expandTableView/InputFieldCell.swift
@@ -12,8 +12,28 @@ class InputFieldCell: UITableViewCell {
 
     @IBOutlet var titleLabel: UILabel!
     
-    @IBOutlet var inputField: UITextField!
+    @IBOutlet var cellHeightConstraint: NSLayoutConstraint!
     
+    @IBOutlet var textView: UITextView!
+    
+    var tableView: UITableView?
+    
+    var placeHolder: String? {
+        
+        willSet {
+            
+            if let placeholder = newValue {
+                
+                self.textView.text = placeholder
+                
+                self.textView.textColor = .gray
+                
+            }
+            
+        }
+        
+    }
+
     override func awakeFromNib() {
         super.awakeFromNib()
     }
@@ -23,4 +43,59 @@ class InputFieldCell: UITableViewCell {
 
     }
 
+}
+
+extension InputFieldCell: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        
+        self.tableView?.beginUpdates()
+        
+        let constant = textView.contentSize.height > 43.5 ? textView.contentSize.height : 43.5
+        
+        self.cellHeightConstraint.constant = constant
+
+        self.tableView?.endUpdates()
+    
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        
+        self.textView.text = nil
+        
+        self.textViewDidChange(textView)
+        
+        if self.textView.textColor == .gray {
+            
+            self.textView.textColor = .white
+            
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        
+        if self.textView.text.isEmpty {
+            
+            self.textView.text = self.placeHolder
+            
+            self.textView.textColor = .gray
+            
+        }
+        
+        textView.scrollRectToVisible(CGRect.zero, animated: false)
+        
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        if text == "\n" {
+            
+            textView.endEditing(true)
+            
+            return false
+        }
+        
+        return true
+    }
+    
 }
